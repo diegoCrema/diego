e5cbd5c32881a1ecb596462018388e488129b76b
crema <crema@LAPTOP-O6PUHB9F.Home>
2023-11-12 23:21:04 +0100

    a=5

--- src/diego/classediego.java
+++ src/diego/classediego.java
@@ -4,7 +4,7 @@ public class classediego {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int a=2;
+int a=5;
 int b=3;
 int somma=a+b;
 System.out.println(somma);
